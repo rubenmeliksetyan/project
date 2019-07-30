93e44afd98306b1fc619d703e8487cb3ef1da4ac
rubenmeliksetyan <mr.supermutant@gmail.com>
2019-07-30 12:11:22 +0400

    property to tenant through tenancy

--- app/Traits/UploadTrait.php
+++ app/Traits/UploadTrait.php
@@ -9,7 +9,6 @@ trait UploadTrait
     public function uploadOne(UploadedFile $uploadedFile, $folder = null, $disk = 'public', $filename = null)
     {
         $name = !is_null($filename) ? $filename : str_random(25);
-
         $file = $uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
 
         return $file;
